adf8de1b5dd26b92baa1d60b039fb01c99fa086d
Jim Burton <jim618@fastmail.co.uk>
2013-10-09 18:18:04 +0100

    initial Mac app and dmg being built

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("MultiBit HD");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
