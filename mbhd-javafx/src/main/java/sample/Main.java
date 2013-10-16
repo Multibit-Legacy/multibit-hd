7071fe2cb16ce9f309fe97826487e76aa5b47075
Gary Rowe <g.rowe@froot.co.uk>
2013-10-16 09:06:54 +0100

    Adding structure for logging

--- mbhd-javafx/src/main/java/sample/Main.java
+++ mbhd-javafx/src/main/java/sample/Main.java
@@ -8,16 +8,16 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("MultiBit HD");
-        primaryStage.setScene(new Scene(root, 600, 400));
-        primaryStage.show();
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+    primaryStage.setTitle("MultiBit HD");
+    primaryStage.setScene(new Scene(root, 600, 400));
+    primaryStage.show();
+  }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
