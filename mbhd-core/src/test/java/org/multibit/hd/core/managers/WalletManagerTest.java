f8b22a77a8f6e930376147e46697ee5804e6d0ed
Gary Rowe <g.rowe@froot.co.uk>
2013-12-12 16:59:45 +0000

    Upgraded unit test asserts to FEST

--- mbhd-core/src/test/java/org/multibit/hd/core/managers/WalletManagerTest.java
+++ mbhd-core/src/test/java/org/multibit/hd/core/managers/WalletManagerTest.java
@@ -17,10 +17,10 @@ package org.multibit.hd.core.managers;
  */
 
 import com.google.bitcoin.core.ECKey;
-import com.google.bitcoin.core.NetworkParameters;
 import com.google.bitcoin.core.Utils;
 import com.google.bitcoin.core.Wallet;
 import com.google.bitcoin.crypto.KeyCrypterScrypt;
+import com.google.bitcoin.params.MainNetParams;
 import org.bitcoinj.wallet.Protos;
 import org.junit.Before;
 import org.junit.Test;
@@ -31,7 +31,7 @@ import java.math.BigInteger;
 import java.util.Collection;
 import java.util.Iterator;
 
-import static junit.framework.Assert.*;
+import static org.fest.assertions.api.Assertions.assertThat;
 
 public class WalletManagerTest {
 
@@ -58,7 +58,7 @@ public class WalletManagerTest {
 
     KeyCrypterScrypt initialKeyCrypter = new KeyCrypterScrypt();
     System.out.println("testCreateProtobufEncryptedWallet - InitialKeyCrypter = " + initialKeyCrypter);
-    Wallet newWallet = new Wallet(NetworkParameters.prodNet(), initialKeyCrypter);
+    Wallet newWallet = new Wallet(MainNetParams.get(), initialKeyCrypter);
     newWallet.setVersion(3); // PROTOBUF_ENCRYPTED
 
 
@@ -84,7 +84,7 @@ public class WalletManagerTest {
     // Get the keys of the wallet and check that all the keys are encrypted.
     Collection<ECKey> keys = newWallet.getKeys();
     for (ECKey key : keys) {
-      assertTrue("Key is not encrypted when it should be", key.isEncrypted());
+      assertThat(key.isEncrypted()).isTrue();
     }
 
     // Save the wallet and read it back in again.
@@ -92,19 +92,19 @@ public class WalletManagerTest {
 
     // Check the wallet and wallet info file exists.
     File newWalletFile = new File(newWalletFilename);
-    assertTrue(newWalletFile.exists());
+    assertThat(newWalletFile.exists()).isTrue();
 
     // Check wallet can be loaded and is still protobuf and encrypted.
     Wallet rebornWallet = walletManager.loadFromFile(newWalletFile);
-    assertNotNull(rebornWallet);
-    assertEquals(BigInteger.ZERO, rebornWallet.getBalance());
-    assertEquals(2, rebornWallet.getKeys().size());
-    assertTrue("Wallet is not of type ENCRYPTED when it should be", rebornWallet.getEncryptionType() == Protos.Wallet.EncryptionType.ENCRYPTED_SCRYPT_AES);
+    assertThat(rebornWallet).isNotNull();
+    assertThat(rebornWallet.getBalance()).isEqualTo(BigInteger.ZERO);
+    assertThat(rebornWallet.getKeys().size()).isEqualTo(2);
+    assertThat(rebornWallet.getEncryptionType()).describedAs("Wallet is not of type ENCRYPTED when it should be").isEqualTo(Protos.Wallet.EncryptionType.ENCRYPTED_SCRYPT_AES);
 
     // Get the keys out the reborn wallet and check that all the keys are encrypted.
     Collection<ECKey> rebornEncryptedKeys = rebornWallet.getKeys();
     for (ECKey key : rebornEncryptedKeys) {
-      assertTrue("Key is not encrypted when it should be", key.isEncrypted());
+      assertThat(key.isEncrypted()).describedAs("Key is not encrypted when it should be").isTrue();
     }
 
     System.out.println("Reborn KeyCrypter = " + rebornWallet.getKeyCrypter());
@@ -115,27 +115,27 @@ public class WalletManagerTest {
     // Get the keys out the reborn wallet and check that all the keys match.
     Collection<ECKey> rebornKeys = rebornWallet.getKeys();
 
-    assertEquals("Wrong number of keys in reborn wallet", 2, rebornKeys.size());
+    assertThat(rebornKeys.size()).describedAs("Wrong number of keys in reborn wallet").isEqualTo(2);
 
     Iterator<ECKey> iterator = rebornKeys.iterator();
     ECKey firstRebornKey = iterator.next();
-    assertTrue("firstRebornKey should now de decrypted but is not", !firstRebornKey.isEncrypted());
+    assertThat(!firstRebornKey.isEncrypted()).describedAs("firstRebornKey should now de decrypted but is not").isTrue();
     // The reborn unencrypted private key bytes should match the original private key.
     byte[] firstRebornPrivateKeyBytes = firstRebornKey.getPrivKeyBytes();
     System.out.println("FileHandlerTest - Reborn decrypted first private key = " + Utils.bytesToHexString(firstRebornPrivateKeyBytes));
 
     for (int i = 0; i < firstRebornPrivateKeyBytes.length; i++) {
-      assertEquals("Byte " + i + " of the reborn first private key did not match the original", originalPrivateKeyBytes1[i], firstRebornPrivateKeyBytes[i]);
+      assertThat(originalPrivateKeyBytes1[i]).describedAs("Byte " + i + " of the reborn first private key did not match the original").isEqualTo(firstRebornPrivateKeyBytes[i]);
     }
 
     ECKey secondRebornKey = iterator.next();
-    assertTrue("secondRebornKey should now de decrypted but is not", !secondRebornKey.isEncrypted());
+    assertThat(!secondRebornKey.isEncrypted()).describedAs("secondRebornKey should now de decrypted but is not").isTrue();
     // The reborn unencrypted private key bytes should match the original private key.
     byte[] secondRebornPrivateKeyBytes = secondRebornKey.getPrivKeyBytes();
     System.out.println("FileHandlerTest - Reborn decrypted second private key = " + Utils.bytesToHexString(secondRebornPrivateKeyBytes));
 
     for (int i = 0; i < secondRebornPrivateKeyBytes.length; i++) {
-      assertEquals("Byte " + i + " of the reborn second private key did not match the original", originalPrivateKeyBytes2[i], secondRebornPrivateKeyBytes[i]);
+      assertThat(originalPrivateKeyBytes2[i]).describedAs("Byte " + i + " of the reborn second private key did not match the original").isEqualTo(secondRebornPrivateKeyBytes[i]);
     }
   }
 
@@ -146,8 +146,8 @@ public class WalletManagerTest {
 
     File walletDirectory = WalletManager.getWalletDirectory(temporaryFile.getParent(), "aName");
 
-    assertEquals(temporaryFile.getParent() + File.separator + "aName", walletDirectory.getAbsolutePath());
-    assertTrue(walletDirectory.isDirectory());
+    assertThat(temporaryFile.getParent() + File.separator + "aName").isEqualTo(walletDirectory.getAbsolutePath());
+    assertThat(walletDirectory.isDirectory()).isTrue();
 
   }
 
@@ -158,7 +158,7 @@ public class WalletManagerTest {
 
     Wallet wallet = walletManager.createSimpleWallet("testPassword");
 
-    assertNotNull(wallet);
+    assertThat(wallet).isNotNull();
   }
 }
 
