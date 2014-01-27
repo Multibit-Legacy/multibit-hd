2e8022bc5437cb85d037d1542a69a3e6611c0eee
Gary Rowe <g.rowe@froot.co.uk>
2014-01-27 16:58:30 +0000

    Reinstated the key to allow IDEs to perform a "where used" and various refactorings across resource bundles more smoothly

--- mbhd-core/src/main/java/org/multibit/hd/core/api/CoreMessageKey.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/api/CoreMessageKey.java
@@ -6,23 +6,38 @@ package org.multibit.hd.core.api;
  * <li>Message keys to use for internationalisation of output from the core module</li>
  * </ul>
  *
+ * <h3>Naming conventions</h3>
+ * <p>Message keys are placed in an enum for type safety.</p>
+ * <p>Message keys have their resource key so that IDEs can maintain a "where used" reference lookup.</p>
+ * <p>Message keys are never concatenated to form larger sentences. Instead use token replacements
+ * within a larger sentence structure.</p>
+ * <p>Resource keys are simply the message key enum name prefixed with "core_" in lower case.</p>
+ *
  * @since 0.0.1
  *  
  */
 public enum CoreMessageKey {
 
   // Core messages
-  START_NETWORK_CONNECTION_ERROR,
-  CHAIN_DOWNLOAD,
-  PEER_COUNT
+  START_NETWORK_CONNECTION_ERROR("core_start_network_connection_error"),
+  CHAIN_DOWNLOAD("core_chain_download"),
+  PEER_COUNT("core_peer_count")
 
+  // End of enum
   ;
+
+  private final String key;
+
+  private CoreMessageKey(String key) {
+    this.key = key;
+  }
 
   /**
    * @return The key for use with the resource bundles
    */
+
   public String getKey() {
-    return "core_"+name().toLowerCase();
+    return key;
   }
 
 }
