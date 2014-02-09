c4bf3d261c867f5dfee010916991836564f6215d
Jim Burton <jim618@fastmail.co.uk>
2014-02-09 20:19:27 +0000

    some improvements to event emission at start up (still not quite right)

--- mbhd-core/src/main/java/org/multibit/hd/core/network/MultiBitPeerEventListener.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/network/MultiBitPeerEventListener.java
@@ -15,24 +15,38 @@ import java.util.List;
 public class MultiBitPeerEventListener implements PeerEventListener {
 
   private static final Logger log = LoggerFactory.getLogger(MultiBitPeerEventListener.class);
-  private int startingBlock = -1;
+  private int numberOfBlocksAtStart = -1;
   private int downloadPercent = 0;
-  private int numberOfConnectedPeers;
+  private int numberOfConnectedPeers = 0;
+
+  private boolean suppressPeerCountMessages = false;
 
   public MultiBitPeerEventListener() {
-    numberOfConnectedPeers = 0;
   }
 
   @Override
   public void onBlocksDownloaded(Peer peer, Block block, int blocksLeft) {
 
-    log.trace("Number of blocks left = {}", blocksLeft);
+    log.debug("Number of blocks left = {}", blocksLeft);
+
+    if (blocksLeft > 0) {
+      // Don't show any peer count messages until the download is complete
+      suppressPeerCountMessages = true;
+    } else {
+      suppressPeerCountMessages = false;
+    }
 
     // Keep track of the download progress
     updateDownloadPercent(blocksLeft);
 
-    // Keep the progress updated
+    // Fire the download percentage
     CoreEvents.fireBitcoinNetworkChangedEvent(BitcoinNetworkSummary.newChainDownloadProgress(downloadPercent));
+
+    if (!suppressPeerCountMessages) {
+      // Switch to showing the peer count
+      CoreEvents.fireBitcoinNetworkChangedEvent(
+              BitcoinNetworkSummary.newNetworkReady(numberOfConnectedPeers));
+    }
   }
 
   @Override
@@ -40,39 +54,51 @@ public class MultiBitPeerEventListener implements PeerEventListener {
 
     log.debug("Chain download started with number of blocks left = {}", blocksLeft);
 
-    // Reset the starting block
-    startingBlock = -1;
+    // Reset the number of blocks at the start of the download
+    numberOfBlocksAtStart = blocksLeft;
+
+    if (blocksLeft > 0) {
+      // Don't show any peer count messages until the download is complete
+      suppressPeerCountMessages = true;
+    } else {
+      suppressPeerCountMessages = false;
+    }
 
     // Keep track of the download progress
     updateDownloadPercent(blocksLeft);
 
-    // Keep the progress updated
-    CoreEvents.fireBitcoinNetworkChangedEvent(BitcoinNetworkSummary.newChainDownloadProgress(downloadPercent));
+    // When the blocks are being downloaded - update the display
+    if (suppressPeerCountMessages) {
+      CoreEvents.fireBitcoinNetworkChangedEvent(BitcoinNetworkSummary.newChainDownloadProgress(downloadPercent));
+    } else {
+      // Switch to showing the peer count
+      CoreEvents.fireBitcoinNetworkChangedEvent(
+              BitcoinNetworkSummary.newNetworkReady(numberOfConnectedPeers));
+    }
   }
 
   @Override
   public void onPeerConnected(Peer peer, int peerCount) {
+    log.debug("(connect) Number of peers = " + peerCount + ", downloadPercent = " + downloadPercent);
 
     numberOfConnectedPeers = peerCount;
 
     // Only show peers after synchronization to avoid confusion
-    if (downloadPercent == 100) {
+    if (!suppressPeerCountMessages) {
       CoreEvents.fireBitcoinNetworkChangedEvent(
-        BitcoinNetworkSummary.newNetworkReady(numberOfConnectedPeers)
-      );
+              BitcoinNetworkSummary.newNetworkReady(numberOfConnectedPeers));
     }
   }
 
   @Override
   public void onPeerDisconnected(Peer peer, int peerCount) {
-
+    log.debug("(disconnect) Number of peers = " + peerCount);
     numberOfConnectedPeers = peerCount;
 
     // Only show peers after synchronization to avoid confusion
-    if (downloadPercent == 100) {
+    if (!suppressPeerCountMessages) {
       CoreEvents.fireBitcoinNetworkChangedEvent(
-        BitcoinNetworkSummary.newNetworkReady(numberOfConnectedPeers)
-      );
+              BitcoinNetworkSummary.newNetworkReady(numberOfConnectedPeers));
     }
   }
 
@@ -97,7 +123,7 @@ public class MultiBitPeerEventListener implements PeerEventListener {
                 if (!(transaction.isTimeLocked() && transaction.getConfidence().getSource() != TransactionConfidence.Source.SELF)) {
                   if (currentWallet.getTransaction(transaction.getHash()) == null) {
                     log.debug("MultiBitHD adding a new pending transaction for the wallet '"
-                      + currentWalletData.get().getWalletId() + "'\n" + transaction.toString());
+                            + currentWalletData.get().getWalletId() + "'\n" + transaction.toString());
                     // The perWalletModelData is marked as dirty.
                     // TODO - mark wallet as dirty ?
                     currentWallet.receivePending(transaction, null);
@@ -132,12 +158,17 @@ public class MultiBitPeerEventListener implements PeerEventListener {
    * @param blocksLeft The number of blocks left to download
    */
   private void updateDownloadPercent(int blocksLeft) {
-
-    if (startingBlock == -1) {
-      startingBlock = blocksLeft;
+    if (numberOfBlocksAtStart == -1) {
+      // We don't have a number of blocks at the start so count down from the blocksLeft figure
+      numberOfBlocksAtStart = blocksLeft;
     }
 
-    downloadPercent = (int) ((1 - ((double) blocksLeft / startingBlock)) * 100);
+    if (blocksLeft == 0 && numberOfBlocksAtStart == 0) {
+      // Nothing to download so we are finished
+      downloadPercent = 100;
+    } else {
+      downloadPercent = (int) ((1 - ((double) blocksLeft / numberOfBlocksAtStart)) * 100);
+    }
   }
 }
 
