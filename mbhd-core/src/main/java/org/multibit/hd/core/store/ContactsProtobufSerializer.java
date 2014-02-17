48aa2acb19e23a5e91c53823caa1008fd19f2d3b
Jim Burton <jim618@fastmail.co.uk>
2014-02-17 11:08:02 +0000

    deleted old MBHDProtos

--- mbhd-core/src/main/java/org/multibit/hd/core/store/ContactsProtobufSerializer.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/store/ContactsProtobufSerializer.java
@@ -23,7 +23,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import com.google.protobuf.TextFormat;
-import org.multibit.contact.MBHDProtos;
+import org.multibit.hd.core.protobuf.MBHDProtos;
 import org.multibit.hd.core.dto.Contact;
 import org.multibit.hd.core.exceptions.ContactsLoadException;
 import org.slf4j.Logger;
