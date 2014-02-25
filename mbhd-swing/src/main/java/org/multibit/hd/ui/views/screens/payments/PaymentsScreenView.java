46d5aad124916309726f0d9c65d3540309a87731
Jim Burton <jim618@fastmail.co.uk>
2014-02-25 15:53:26 +0000

    null pointer fix

--- mbhd-swing/src/main/java/org/multibit/hd/ui/views/screens/payments/PaymentsScreenView.java
+++ mbhd-swing/src/main/java/org/multibit/hd/ui/views/screens/payments/PaymentsScreenView.java
@@ -109,7 +109,7 @@ public class PaymentsScreenView extends AbstractScreenView<PaymentsScreenModel> 
 
     // Play a sound the first time a transaction is received
     // TODO some more filtering required - just set to play when it confirms for the first time for now
-    if (transactionSeenEvent.getDepthInBlocks() == 1 && transactionSeenEvent.getValue().compareTo(BigInteger.ZERO) >0) {
+    if (transactionSeenEvent.getDepthInBlocks() == 1 && transactionSeenEvent.getValue() != null && transactionSeenEvent.getValue().compareTo(BigInteger.ZERO) >0) {
       Sounds.playReceiveBitcoin();
     }
 
