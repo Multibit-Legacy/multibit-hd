1f44caa53794aa25a6de3a2bb90999faa0d882dd
Gary Rowe <g.rowe@froot.co.uk>
2014-03-20 15:35:00 +0000

    Tidied up formatting

--- mbhd-core/src/main/java/org/multibit/hd/core/export/PaymentRequestDataHeaderEntryConverter.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/export/PaymentRequestDataHeaderEntryConverter.java
@@ -7,23 +7,23 @@ import org.multibit.hd.core.dto.PaymentRequestData;
  * Create a CSVEntryConverter for the header values in the CSV
  */
 public class PaymentRequestDataHeaderEntryConverter implements CSVEntryConverter<PaymentRequestData> {
-    @Override
-    public String[] convertEntry(PaymentRequestData paymentRequestData) {
-        String[] columns = new String[4];
+  @Override
+  public String[] convertEntry(PaymentRequestData paymentRequestData) {
+    String[] columns = new String[4];
 
-        // Date.
-        columns[0] = "Date";
+    // Date.
+    columns[0] = "Date";
 
-        // Description.
-        columns[1] = "Description";
+    // Description.
+    columns[1] = "Description";
 
-        // Amount in BTC.
-        columns[2] = "Amount BTC";
+    // Amount in BTC.
+    columns[2] = "Amount BTC";
 
-        // Amount in fiat
-        columns[3] = "Amount fiat";
+    // Amount in fiat
+    columns[3] = "Amount fiat";
 
-        return columns;
-    }
+    return columns;
+  }
 }
 
