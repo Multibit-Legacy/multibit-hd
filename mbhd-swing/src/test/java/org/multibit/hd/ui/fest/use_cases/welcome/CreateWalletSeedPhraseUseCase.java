e64757014244f7c44aae20ec925c45a11b7dbff4
Gary Rowe <g.rowe@froot.co.uk>
2014-04-18 18:13:00 +0100

    Improved seed phrase interactivity testing

--- mbhd-swing/src/test/java/org/multibit/hd/ui/fest/use_cases/welcome/CreateWalletSeedPhraseUseCase.java
+++ mbhd-swing/src/test/java/org/multibit/hd/ui/fest/use_cases/welcome/CreateWalletSeedPhraseUseCase.java
@@ -5,6 +5,8 @@ import org.multibit.hd.ui.fest.use_cases.AbstractFestUseCase;
 import org.multibit.hd.ui.languages.Languages;
 import org.multibit.hd.ui.languages.MessageKey;
 
+import static org.fest.assertions.Assertions.assertThat;
+
 /**
  * <p>Use case to provide the following to FEST testing:</p>
  * <ul>
@@ -47,8 +49,7 @@ public class CreateWalletSeedPhraseUseCase extends AbstractFestUseCase {
       .comboBox(MessageKey.SEED_SIZE.getKey())
       .requireEnabled()
       .requireVisible()
-      .requireSelection(0)
-      .click();
+      .requireSelection(0);
 
     window
       .textBox(MessageKey.SEED_PHRASE.getKey())
@@ -63,12 +64,92 @@ public class CreateWalletSeedPhraseUseCase extends AbstractFestUseCase {
       .requireVisible();
 
     // Verify interactions
-    //verifySelectWalletEnabled();
+    verifySeedPhraseHides();
+    verifySeedPhraseRefreshes();
+    verifySeedPhraseSizeChanges();
 
     // OK to proceed
     window
       .button(MessageKey.NEXT.getKey())
       .click();
+
+  }
+
+  private void verifySeedPhraseHides() {
+
+    String seedPhrase1 = window
+      .textBox(MessageKey.SEED_PHRASE.getKey())
+      .text();
+
+    // Hide the phrase
+    window
+      .button(MessageKey.HIDE.getKey())
+      .click();
+
+    String seedPhrase2 = window
+      .textBox(MessageKey.SEED_PHRASE.getKey())
+      .text();
+
+    assertThat(seedPhrase2).isNotEqualTo(seedPhrase1);
+
+    // Show the phrase
+    window
+      .button(MessageKey.HIDE.getKey())
+      .click();
+
+    String seedPhrase3 = window
+      .textBox(MessageKey.SEED_PHRASE.getKey())
+      .text();
+
+    assertThat(seedPhrase3).isEqualTo(seedPhrase1);
+
+  }
+
+  private void verifySeedPhraseRefreshes() {
+
+    String seedPhrase1 = window
+      .textBox(MessageKey.SEED_PHRASE.getKey())
+      .text();
+
+    window
+      .button(MessageKey.REFRESH.getKey())
+      .click();
+
+    String seedPhrase2 = window
+      .textBox(MessageKey.SEED_PHRASE.getKey())
+      .text();
+
+    assertThat(seedPhrase2).isNotEqualTo(seedPhrase1);
+
+  }
+
+  private void verifySeedPhraseSizeChanges() {
+
+    String seedPhrase1 = window
+      .textBox(MessageKey.SEED_PHRASE.getKey())
+      .text();
+
+    window
+      .comboBox(MessageKey.SEED_SIZE.getKey())
+      .selectItem(1);
+
+    String seedPhrase2 = window
+      .textBox(MessageKey.SEED_PHRASE.getKey())
+      .text();
+
+    // Expect at least 24 extra characters (6 words * 4 minimum length)
+    assertThat(seedPhrase2.length()).isGreaterThan(seedPhrase1.length()+24);
+
+    window
+      .comboBox(MessageKey.SEED_SIZE.getKey())
+      .selectItem(2);
+
+    String seedPhrase3 = window
+      .textBox(MessageKey.SEED_PHRASE.getKey())
+      .text();
+
+    // Expect at least 24 extra characters (6 words * 4 minimum length)
+    assertThat(seedPhrase3.length()).isGreaterThan(seedPhrase2.length()+24);
 
   }
 
