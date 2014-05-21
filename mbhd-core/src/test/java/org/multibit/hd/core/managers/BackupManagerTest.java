0ff7b6c86990e67409af165b3a7654fde59996b9
Jim Burton <jim618@fastmail.co.uk>
2014-05-21 17:52:23 +0100

    fixed test

--- mbhd-core/src/test/java/org/multibit/hd/core/managers/BackupManagerTest.java
+++ mbhd-core/src/test/java/org/multibit/hd/core/managers/BackupManagerTest.java
@@ -75,6 +75,7 @@ public class BackupManagerTest {
     // Wallet manager does not initiate the backup
     BackupManager.INSTANCE.createRollingBackup(walletSummary, password);
     BackupManager.INSTANCE.createLocalBackup(walletSummary.getWalletId(), password);
+    BackupManager.INSTANCE.createCloudBackup(walletSummary.getWalletId(), password);
 
     // Check there are initially a single wallet backup for the wallet id of the created wallet
     List<BackupSummary> localBackups = BackupManager.INSTANCE.getLocalZipBackups(walletSummary.getWalletId());
@@ -91,6 +92,8 @@ public class BackupManagerTest {
     // Backup the wallet.
     // This zips the wallet root directory and adds a timestamp, then saves the file in both the local and cloud backup directories
     File localBackupFile = BackupManager.INSTANCE.createLocalBackup(walletSummary.getWalletId(), password);
+    BackupManager.INSTANCE.createCloudBackup(walletSummary.getWalletId(), password);
+
 
     // Check that a backup copy has been saved in the local backup directory
     localBackups = BackupManager.INSTANCE.getLocalZipBackups(walletSummary.getWalletId());
