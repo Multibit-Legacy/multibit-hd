011b79c175168f5b806662351b998cfe33dbf79c
Gary Rowe <g.rowe@froot.co.uk>
2014-07-03 21:23:18 +0100

    #104 Moved protocol handler to post install Simplified launcher to remove thread Fixed Maven issues surrounding installer build

---
+++ mbhd-install/src/main/java/Empty.java
@@ -0,0 +1,11 @@
+/**
+ * <p>Empty class to provide the following to installer:</p>
+ * <ul>
+ * <li>Something empty to compile</li>
+ * </ul>
+ *
+ * @since 0.0.1
+ *  
+ */
+public class Empty {
+}
--- mbhd-install/src/main/jwrapper/org/multibit/hd/installer/MultiBitHDLauncher.java
+++ mbhd-install/src/main/jwrapper/org/multibit/hd/installer/MultiBitHDLauncher.java
@@ -1,23 +1,15 @@
 package org.multibit.hd.installer;
 
-import jwrapper.JWConstants;
-import jwrapper.hidden.events.JWOSXEventListener;
+import com.google.common.base.Optional;
 import jwrapper.jwutils.JWMacOS;
-import jwrapper.jwutils.JWSystem;
 import jwrapper.jwutils.JWWindowsOS;
-import jwrapper.updater.JWApp;
-import jwrapper.updater.JWLaunchProperties;
-import org.multibit.hd.core.concurrent.SafeExecutors;
 import org.multibit.hd.ui.MultiBitHD;
 import utils.ostools.OS;
-
-import java.io.IOException;
-import java.util.concurrent.ExecutorService;
 
 /**
  * <p>JWrapper launcher to provide the following to application:</p>
  * <ul>
- * <li>Native OS integration for protocol handling</li>
+ * <li>Respond to application start either by click or protocol handler</li>
  * </ul>
  *
  * @since 0.0.1
@@ -25,66 +17,24 @@ import java.util.concurrent.ExecutorService;
  */
 public class MultiBitHDLauncher {
 
-  /**
-   * Ensure we only deal with one Bitcoin URI at a time
-   */
-  private static final ExecutorService bitcoinUriExecutorService = SafeExecutors.newSingleThreadExecutor("bitcoin-uri");
-
   public static void main(String[] args) throws Exception {
 
-    // TODO Need some configuration to prevent overwriting user's choice
-    registerProtocolHandler();
-
     // Check for any URLs when launching
-    String url = JWLaunchProperties.getPropertyPossiblyNull(JWLaunchProperties.PROP_URL_HANDLER_OPEN);
+    Optional<String> url = Optional.absent();
+    if (OS.isMacOS()) {
+      url = Optional.fromNullable(JWMacOS.getRequestedURL());
+    }
+    if (OS.isWindows()) {
+      url = Optional.fromNullable(JWWindowsOS.getRequestedURL());
+    }
 
     // Multiple instances will handover correctly
-    if (url != null) {
-      MultiBitHD.main(new String[]{url});
+    if (url.isPresent()) {
+      MultiBitHD.main(new String[]{url.get()});
     } else {
       MultiBitHD.main(new String[]{});
     }
 
-  }
-
-  /**
-   * Handles the process of registering this application as a protocol handler
-   */
-  private static void registerProtocolHandler() throws IOException, InterruptedException {
-
-    if (OS.isMacOS()) {
-
-      String appBundle = JWSystem.getAppBundleName();
-      String bundleID = JWConstants.buildOsxDomainFromBundle(appBundle);
-
-      JWMacOS.registerAppAsURLHandler("bitcoin", bundleID);
-
-      JWMacOS.setOSXEventListener(new JWOSXEventListener() {
-        public void openURL(final String url) {
-
-          bitcoinUriExecutorService.submit(new Runnable() {
-            @Override
-            public void run() {
-              try {
-                // Multiple instances will handover correctly
-                MultiBitHD.main(new String[]{url});
-              } catch (Exception e) {
-                // Nothing to be done if this fails
-                e.printStackTrace();
-              }
-
-            }
-          });
-        }
-      });
-    } else if (OS.isWindows()) {
-
-      String vappName = JWApp.getMyVirtualApp().getUserVisibleName();
-
-      JWWindowsOS.registerURLSchemeForVirtualApp("bitcoin", vappName);
-    }
-
-    // TODO Find a suitable solution for Linux
   }
 
 }
