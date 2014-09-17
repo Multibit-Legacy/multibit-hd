4f878073c535fc6c9c2260257da24beab80dd349
Jim Burton <jim618@fastmail.co.uk>
2014-09-17 16:10:04 +0100

    sendBySelf rename

--- mbhd-core/src/main/java/org/multibit/hd/core/managers/TransactionInfoSentBySelfProvider.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/managers/TransactionInfoSentBySelfProvider.java
@@ -2,6 +2,7 @@ package org.multibit.hd.core.managers;
 
 import com.google.bitcoin.core.Transaction;
 import com.google.bitcoin.core.Wallet;
+import org.multibit.hd.brit.services.TransactionConfidenceSentBySelfProvider;
 import org.multibit.hd.brit.services.TransactionSentBySelfProvider;
 import org.multibit.hd.core.dto.WalletId;
 import org.multibit.hd.core.services.CoreServices;
@@ -11,7 +12,7 @@ import org.multibit.hd.core.store.TransactionInfo;
 /**
  *  <p>Class to provide the following to WalletManager:<br>
  *  <ul>
- *  <li>Provides sentBySelf information using Transacitoninfo</li>
+ *  <li>Provides sentBySelf information using either Transacitoninfo or the transactionConfidence</li>
  *  </ul>
  *  Example:<br>
  *  <pre>
@@ -23,13 +24,25 @@ public class TransactionInfoSentBySelfProvider implements TransactionSentBySelfP
 
   private WalletId walletId;
 
+  /**
+   * Use a standard TransactionConfidenceSentBySelfProvider to catch the case where the transaction
+   * confidence is available but the transaction info was written prior to the extra sentBySelf boolean
+   */
+  TransactionSentBySelfProvider transactionConfidenceSentBySelfProvider;
+
   public TransactionInfoSentBySelfProvider(WalletId walletId) {
       this.walletId = walletId;
+    transactionConfidenceSentBySelfProvider = new TransactionConfidenceSentBySelfProvider();
   }
 
   @Override
   public boolean isSentBySelf(Wallet wallet, Transaction transaction) {
-    // Use sentBySelf on corresponding transactionInfo
+    // If the transactionConfidence tells us it was sent by self then we are done
+    if (transactionConfidenceSentBySelfProvider.isSentBySelf(wallet, transaction)) {
+      return true;
+    }
+
+    // Otherwise use sentBySelf on corresponding transactionInfo
     String transactionHashAsString = transaction.getHashAsString();
     WalletService walletService = CoreServices.getOrCreateWalletService(walletId);
 
