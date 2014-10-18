f411a21dd5aed66fdf2b681ac08e198e1aa5e681
Gary Rowe <g.rowe@froot.co.uk>
2014-10-18 10:29:30 +0100

    #194 Reinstated FEST tests for verification

--- mbhd-swing/src/test/java/org/multibit/hd/ui/fest/requirements/ToolsScreenRequirements.java
+++ mbhd-swing/src/test/java/org/multibit/hd/ui/fest/requirements/ToolsScreenRequirements.java
@@ -3,8 +3,13 @@ package org.multibit.hd.ui.fest.requirements;
 import com.google.common.collect.Maps;
 import org.fest.swing.fixture.FrameFixture;
 import org.multibit.hd.ui.fest.use_cases.sidebar.tools.ShowToolsScreenUseCase;
+import org.multibit.hd.ui.fest.use_cases.sidebar.tools.about.ShowThenCancelAboutUseCase;
 import org.multibit.hd.ui.fest.use_cases.sidebar.tools.change_password.ShowThenCancelChangePasswordUseCase;
 import org.multibit.hd.ui.fest.use_cases.sidebar.tools.change_password.VerifyChangePasswordUseCase;
+import org.multibit.hd.ui.fest.use_cases.sidebar.tools.empty_wallet.ShowThenCancelEmptyWalletUseCase;
+import org.multibit.hd.ui.fest.use_cases.sidebar.tools.repair_wallet.ShowThenCancelRepairWalletUseCase;
+import org.multibit.hd.ui.fest.use_cases.sidebar.tools.sign_message.ShowThenFinishSignMessageUseCase;
+import org.multibit.hd.ui.fest.use_cases.sidebar.tools.verify_message.ShowThenFinishVerifyMessageUseCase;
 
 import java.util.Map;
 
@@ -26,22 +31,21 @@ public class ToolsScreenRequirements {
     // Show the tools screen
     new ShowToolsScreenUseCase(window).execute(parameters);
 
-// TODO Reinstate these
-//    // Show then cancel the "repair wallet" wizard
-//    // Verifying the repair will take too long
-//    new ShowThenCancelRepairWalletUseCase(window).execute(parameters);
-//
-//    // Show the "verify message" wizard
-//    new ShowThenFinishVerifyMessageUseCase(window).execute(parameters);
-//
-//    // Show then cancel the "empty wallet" wizard
-//    new ShowThenCancelEmptyWalletUseCase(window).execute(parameters);
-//
-//    // Show the "sign message" wizard
-//    new ShowThenFinishSignMessageUseCase(window).execute(parameters);
-//
-//    // Show then cancel the "about" wizard
-//    new ShowThenCancelAboutUseCase(window).execute(parameters);
+    // Show then cancel the "repair wallet" wizard
+    // Verifying the repair will take too long
+    new ShowThenCancelRepairWalletUseCase(window).execute(parameters);
+
+    // Show the "verify message" wizard
+    new ShowThenFinishVerifyMessageUseCase(window).execute(parameters);
+
+    // Show then cancel the "empty wallet" wizard
+    new ShowThenCancelEmptyWalletUseCase(window).execute(parameters);
+
+    // Show the "sign message" wizard
+    new ShowThenFinishSignMessageUseCase(window).execute(parameters);
+
+    // Show then cancel the "about" wizard
+    new ShowThenCancelAboutUseCase(window).execute(parameters);
 
     // Verify the "change credentials" wizard
     new ShowThenCancelChangePasswordUseCase(window).execute(parameters);
