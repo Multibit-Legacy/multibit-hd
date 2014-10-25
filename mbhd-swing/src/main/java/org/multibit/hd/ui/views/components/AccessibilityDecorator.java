50f48cd0d8c60fae6d57d11adc51355024b9918d
Gary Rowe <g.rowe@froot.co.uk>
2014-10-25 17:18:20 +0100

    #167 Removed middle click support due to confusing behaviour with double clipboards

--- mbhd-swing/src/main/java/org/multibit/hd/ui/views/components/AccessibilityDecorator.java
+++ mbhd-swing/src/main/java/org/multibit/hd/ui/views/components/AccessibilityDecorator.java
@@ -3,13 +3,8 @@ package org.multibit.hd.ui.views.components;
 import org.multibit.hd.core.dto.CoreMessageKey;
 import org.multibit.hd.ui.languages.Languages;
 import org.multibit.hd.ui.languages.MessageKey;
-import org.multibit.hd.ui.utils.ClipboardUtils;
 
 import javax.swing.*;
-import javax.swing.text.JTextComponent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 /**
  * <p>Decorator to provide the following to application:</p>
@@ -20,8 +15,6 @@ import java.awt.event.MouseListener;
  * @since 0.0.1
  */
 public class AccessibilityDecorator {
-
-  private static final String MIDDLE_CLICK_LISTENER = "Middle click listener";
 
   /**
    * <p>Full FEST and Accessibility support (tooltip and description)</p>
@@ -42,8 +35,6 @@ public class AccessibilityDecorator {
     component.getAccessibleContext().setAccessibleName(Languages.safeText(nameKey));
     component.getAccessibleContext().setAccessibleDescription(Languages.safeText(tooltipKey));
 
-    applyMiddleClickPaste(component);
-
   }
 
   /**
@@ -59,8 +50,6 @@ public class AccessibilityDecorator {
 
     // Ensure Accessibility API can find it
     component.getAccessibleContext().setAccessibleName(Languages.safeText(nameKey));
-
-    applyMiddleClickPaste(component);
 
   }
 
@@ -83,8 +72,6 @@ public class AccessibilityDecorator {
     component.getAccessibleContext().setAccessibleName(Languages.safeText(nameKey));
     component.getAccessibleContext().setAccessibleDescription(Languages.safeText(tooltipKey));
 
-    applyMiddleClickPaste(component);
-
   }
 
   /**
@@ -100,57 +87,6 @@ public class AccessibilityDecorator {
 
     // Ensure Accessibility API can find it
     component.getAccessibleContext().setAccessibleName(Languages.safeText(nameKey));
-
-    applyMiddleClickPaste(component);
-
-  }
-
-  /**
-   * @param component The Swing component to decorate (if it is a text component)
-   */
-  private static void applyMiddleClickPaste(JComponent component) {
-
-    // Support middle click
-    if (component instanceof JTextComponent) {
-      for (MouseListener mouseListener : component.getMouseListeners()) {
-        if (MIDDLE_CLICK_LISTENER.equals(mouseListener.toString())) {
-          // Avoid decorating multiple times (e.g. private notes becoming public notes)
-          return;
-        }
-      }
-      component.addMouseListener(getMiddleClickPasteMouseListener((JTextComponent) component));
-    }
-
-  }
-
-  /**
-   * @return A mouse listener that initiates the standard component paste behaviour for middle click on Unix
-   */
-  private static MouseListener getMiddleClickPasteMouseListener(final JTextComponent textComponent) {
-
-    return new MouseAdapter() {
-
-      @Override
-      public String toString() {
-        return MIDDLE_CLICK_LISTENER;
-      }
-
-      @Override
-      public void mouseClicked(MouseEvent e) {
-
-        switch (e.getButton()) {
-          case MouseEvent.BUTTON2:
-            int onmask = MouseEvent.ALT_DOWN_MASK;
-            if ((e.getModifiersEx() & onmask) == onmask)
-            // Support paste from middle click on Unix systems
-            {
-              // Use the standard paste mechanism
-              // Updates can be detected through a DocumentListener on the component
-              ClipboardUtils.pasteStringFromClipboard(textComponent);
-            }
-        }
-      }
-    };
 
   }
 
