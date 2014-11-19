e9406244b37449055aa6750d16a5519551343f6b
Gary Rowe <g.rowe@froot.co.uk>
2014-11-19 07:42:38 +0000

    #246 Introduced and pruned various exchanges based on availability of rates and site

--- mbhd-core/src/main/java/org/multibit/hd/core/exchanges/ExchangeKey.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/exchanges/ExchangeKey.java
@@ -4,18 +4,25 @@ import com.google.common.base.Optional;
 import com.xeiam.xchange.Exchange;
 import com.xeiam.xchange.ExchangeFactory;
 import com.xeiam.xchange.anx.v2.ANXExchange;
+import com.xeiam.xchange.bitbay.BitbayExchange;
 import com.xeiam.xchange.bitcurex.BitcurexExchange;
 import com.xeiam.xchange.bitfinex.v1.BitfinexExchange;
+import com.xeiam.xchange.bitmarket.BitMarketExchange;
 import com.xeiam.xchange.bitstamp.BitstampExchange;
 import com.xeiam.xchange.btcchina.BTCChinaExchange;
 import com.xeiam.xchange.btce.v3.BTCEExchange;
+import com.xeiam.xchange.btctrade.BTCTradeExchange;
 import com.xeiam.xchange.bter.BTERExchange;
 import com.xeiam.xchange.campbx.CampBXExchange;
+import com.xeiam.xchange.cexio.CexIOExchange;
 import com.xeiam.xchange.coinbase.CoinbaseExchange;
+import com.xeiam.xchange.cryptonit.v2.CryptonitExchange;
 import com.xeiam.xchange.cryptotrade.CryptoTradeExchange;
-import com.xeiam.xchange.justcoin.JustcoinExchange;
+import com.xeiam.xchange.hitbtc.HitbtcExchange;
 import com.xeiam.xchange.kraken.KrakenExchange;
+import com.xeiam.xchange.lakebtc.LakeBTCExchange;
 import com.xeiam.xchange.oer.OERExchange;
+import com.xeiam.xchange.okcoin.OkCoinExchange;
 import com.xeiam.xchange.vaultofsatoshi.VaultOfSatoshiExchange;
 import org.multibit.hd.core.config.Configurations;
 
@@ -37,25 +44,42 @@ public enum ExchangeKey {
    */
   NONE(""),
   ANX(ANXExchange.class.getName()),
+  // ATLASATS(AtlasATS.class.getName()), // No 2.1.0 release
+  BITBAY(BitbayExchange.class.getName()),
   // BITCOIN_AVERAGE(BitcoinAverage.class.getName()), // Causes problems with enum creation
   // BITCOIN_CHARTS(BitcoinChartsExchange.class.getName()), // Aggregator over exchanges
   // BITCOINIUM(BitcoiniumExchange.class.getName()), // No currency pair lookup
   BITCUREX(BitcurexExchange.class.getName()),
   BITFINEX(BitfinexExchange.class.getName()),
+  // BIT_KONAN(BitKonanExchange.class.getName()), // No market rates
+  BITMARKET(BitMarketExchange.class.getName()),
   BITSTAMP(BitstampExchange.class.getName()),
+  // BITTREX(BittrexExchange.class.getName()), // Rates broken
+  // BITVC(BitVcExchange.class.getName()), // No exchange URL
   // BLOCKCHAIN(BlockchainExchange.class.getName()), // Not a rate supplier
+  //BTC_CENTRAL(BTCCentralExchange.class.getName()),// No 2.1.0 release
   BTC_CHINA(BTCChinaExchange.class.getName()),
-  BTC_E(BTCEExchange.class.getName()),
-  BTER(BTERExchange.class.getName()),
   CAMPBX(CampBXExchange.class.getName()),
+  BTC_E(BTCEExchange.class.getName()),
+  BTC_TRADE(BTCTradeExchange.class.getName()),
+  BTER(BTERExchange.class.getName()),
   // CA_VIRTEX(VirtExExchange.class.getName()), // Broken
-  // CEXIO(CexIOExchange.class.getName()), // Weird GHS/NMC combo
+  CEXIO(CexIOExchange.class.getName()), // Weird GHS/NMC combo
   COINBASE(CoinbaseExchange.class.getName()), // No dynamic currency pair lookup
+  // COINSETTER(CoinSetterExchange.class.getName()),// No 2.1.0 release
   // COINFLOOR(CoinfloorExchange.class.getName()), // Requires non-trivial registration
+  CRYPTONIT(CryptonitExchange.class.getName()),
   CRYPTO_TRADE(CryptoTradeExchange.class.getName()),
-  JUSTCOIN(JustcoinExchange.class.getName()),
+  //CRYPTSY(CryptsyExchange.class.getName()),// Several broken currencies
+  HITBTC(HitbtcExchange.class.getName()),// GBP feed broken, EUR and USD OK
+  // IT_BIT(ItBitExchange.class.getName()),// No working feed
+  // JUSTCOIN(JustcoinExchange.class.getName()), // No exchange URL at 2.1.0
   KRAKEN(KrakenExchange.class.getName()),
+  LAKE_BTC(LakeBTCExchange.class.getName()),
+  // MINT_PAL(MintPalExchange.class.getName()),// Feed broken (no response)
+  OK_COIN(OkCoinExchange.class.getName()),
   OPEN_EXCHANGE_RATES(OERExchange.class.getName()),
+  // POLONIEX(PoloniexExchange.class.getName()),// No working feed
   VAULT_OF_SATOSHI(VaultOfSatoshiExchange.class.getName()),
   // VIRCUREX(VircurexExchange.class.getName()), // Broken
 
