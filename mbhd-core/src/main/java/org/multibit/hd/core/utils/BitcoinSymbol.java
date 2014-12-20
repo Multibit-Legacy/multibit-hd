0599963c5706c53753d358856dfeb8325becc903
Jim Burton <jim618@fastmail.co.uk>
2014-12-20 11:45:48 +0000

    reordered units

--- mbhd-core/src/main/java/org/multibit/hd/core/utils/BitcoinSymbol.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/utils/BitcoinSymbol.java
@@ -59,13 +59,6 @@ public enum BitcoinSymbol {
    */
   UXBT("\u00b5XBT", "µXBT", new BigDecimal(1_000_000), 2),
   /**
-   * Subject of much debate (see <a href="http://www.reddit.com/r/Bitcoin/comments/1rmto3/its_bits/">this Reddit article</a>)
-   * However, a "bit" is already used for measuring data transmission and reusing it here would be confusing
-   *
-   * Jim - added in due to popular demand
-   */
-  BITS("bits", "bits", new BigDecimal(1_000_000), 2),
-  /**
    * The <a href="http://bitcoinsymbol.org">Ecogex alternative symbol</a>
    */
   ECO("\u0243", "\u0243", BigDecimal.ONE, 8),
@@ -78,6 +71,14 @@ public enum BitcoinSymbol {
    */
   UECO("\u00b5\u0243", "\u00b5\u0243", new BigDecimal(1_000_000), 2),
 
+  /**
+   * Subject of much debate (see <a href="http://www.reddit.com/r/Bitcoin/comments/1rmto3/its_bits/">this Reddit article</a>)
+   * However, a "bit" is already used for measuring data transmission and reusing it here would be confusing
+   *
+   * Jim - added in due to popular demand
+   */
+  BITS("bits", "bits", new BigDecimal(1_000_000), 2),
+  
   /**
    * The smallest possible unit in the current version of Bitcoin
    */
