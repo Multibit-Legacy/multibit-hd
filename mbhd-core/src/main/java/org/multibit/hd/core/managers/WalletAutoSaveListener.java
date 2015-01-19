e206dbfb268690c8a4dc6ebc239f7769f169a91f
Jim Burton <jim618@fastmail.co.uk>
2015-01-19 11:37:43 +0000

    #322 rename wallet file to temp before secure delete

--- mbhd-core/src/main/java/org/multibit/hd/core/managers/WalletAutoSaveListener.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/managers/WalletAutoSaveListener.java
@@ -1,20 +1,19 @@
 package org.multibit.hd.core.managers;
 
 import com.google.common.base.Optional;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import org.bitcoinj.utils.Threading;
 import org.bitcoinj.wallet.WalletFiles;
 import org.multibit.hd.core.crypto.EncryptedFileReaderWriter;
 import org.multibit.hd.core.dto.WalletId;
 import org.multibit.hd.core.dto.WalletSummary;
 import org.multibit.hd.core.exceptions.WalletSaveException;
+import org.multibit.hd.core.files.SecureFiles;
 import org.multibit.hd.core.services.BackupService;
 import org.multibit.hd.core.services.CoreServices;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.concurrent.locks.ReentrantLock;
+import java.io.IOException;
 
 /**
  * <p>Listener to provide the following to WalletManager:</p>
@@ -23,46 +22,23 @@ import java.util.concurrent.locks.ReentrantLock;
  * </ul>
  * </p>
  */
-@SuppressFBWarnings({"UL_UNRELEASED_LOCK"})
+
 public class WalletAutoSaveListener implements WalletFiles.Listener {
 
   private static final Logger log = LoggerFactory.getLogger(WalletAutoSaveListener.class);
 
-  private final ReentrantLock walletSaveLock = Threading.lock("wallet-save");
-
-  private long previousBeforeAutosaveTime = 0;
-
-  private static final long MAXIMUM_WALLET_SAVE_LOCK_TIME = 5000; // millisecond
-
   @Override
   public void onBeforeAutoSave(File tempFile) {
-    long beforeAutoSaveTime = System.currentTimeMillis();
 
-    log.debug("At time: {} .Auto-saving wallet to tempFile:\n'{}'", beforeAutoSaveTime, tempFile.getAbsolutePath());
+    log.debug("Auto-saving wallet to tempFile:\n'{}'", tempFile.getAbsolutePath());
 
-
-    // If the last time the wallet save was locked is a while ago then unlock
-    // (this is to prevent an IOException in bitcoinj from preventing a wallet save unlock)
-    if (beforeAutoSaveTime - previousBeforeAutosaveTime > MAXIMUM_WALLET_SAVE_LOCK_TIME) {
-      if (walletSaveLock.isLocked()) {
-        log.debug("Unlocking walletSaveLock due to timeout");
-        walletSaveLock.unlock();
-      }
-    }
-
-    // Lock the wallet save - this also locks the bitcoinj wallet save
-    log.debug("Locking the walletSaveLock");
-    walletSaveLock.lock();
-
-    // lock was successful - remember when this occurred
-    previousBeforeAutosaveTime = beforeAutoSaveTime;
   }
 
   @Override
   public void onAfterAutoSave(File newlySavedFile) {
-    try {
       log.debug("Wallet auto-saved to newlySavedFile:\n'{}'", newlySavedFile.getAbsolutePath());
 
+    try {
       Optional<WalletSummary> walletSummary = WalletManager.INSTANCE.getCurrentWalletSummary();
 
       if (walletSummary.isPresent()) {
@@ -74,9 +50,17 @@ public class WalletAutoSaveListener implements WalletFiles.Listener {
           throw new WalletSaveException("The password specified is not the password for the wallet saved in '" + newlySavedFile.getAbsolutePath() + "'");
         }
 
+        // Work out what the encrypted wallet filename will be
+        File newlySavedFileWithAES =  new File(newlySavedFile.getAbsoluteFile() + WalletManager.MBHD_AES_SUFFIX);
+
+        // Rename the wallet to a temp file to reduce contention on secure delete
+        File directory = newlySavedFile.getAbsoluteFile().getParentFile();
+        File tempWalletToDelete = SecureFiles.createTemporaryFilename("wallet", null, directory);
+        newlySavedFile.renameTo(tempWalletToDelete);
+
         // Save an encrypted copy of the wallet
         CharSequence password = walletSummary.get().getWalletPassword().getPassword();
-        File encryptedWalletFile = EncryptedFileReaderWriter.makeAESEncryptedCopyAndDeleteOriginal(newlySavedFile, password);
+        File encryptedWalletFile = EncryptedFileReaderWriter.makeAESEncryptedCopyAndDeleteOriginal(tempWalletToDelete, newlySavedFileWithAES, password);
         if (encryptedWalletFile != null && encryptedWalletFile.exists()) {
           log.debug("Save encrypted copy of wallet (size: {} bytes) as:\n'{}'", encryptedWalletFile.length(), encryptedWalletFile.getAbsolutePath());
         } else {
@@ -91,10 +75,8 @@ public class WalletAutoSaveListener implements WalletFiles.Listener {
       } else {
         log.error("No AES wallet encryption nor backups created as there was no wallet data to backup.");
       }
-    } finally {
-      // Release the wallet save lock
-      log.debug("Unlocking the walletSaveLock");
-      walletSaveLock.unlock();
+    } catch (IOException ioe) {
+      throw new WalletSaveException("Could not save wallet " + newlySavedFile.getAbsolutePath(), ioe);
     }
   }
 }
