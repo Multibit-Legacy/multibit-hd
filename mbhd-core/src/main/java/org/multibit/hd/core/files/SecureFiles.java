059c760bc4092e2573aa9836ff85660f8c5ab4fc
Jim Burton <jim618@fastmail.co.uk>
2015-01-30 14:37:46 +0000

    #377 reinstated slowSecureDelete for Windows

--- mbhd-core/src/main/java/org/multibit/hd/core/files/SecureFiles.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/files/SecureFiles.java
@@ -2,6 +2,8 @@ package org.multibit.hd.core.files;
 
 import com.google.common.base.Preconditions;
 import com.google.common.io.ByteStreams;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import org.bitcoinj.core.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -28,6 +30,29 @@ public class SecureFiles {
 
   private static SecureRandom secureRandom = new SecureRandom();
 
+   // Nonsense bytes to fill up deleted files - these have no meaning.
+  static final byte[] NONSENSE_BYTES = new byte[]{(byte) 0xF0, (byte) 0xA6, (byte) 0x55, (byte) 0xAA, (byte) 0x33,
+    (byte) 0x77, (byte) 0x33, (byte) 0x37, (byte) 0x12, (byte) 0x34, (byte) 0x56, (byte) 0x78, (byte) 0xC2, (byte) 0xB3,
+    (byte) 0xA4, (byte) 0x9A, (byte) 0x30, (byte) 0x7F, (byte) 0xE5, (byte) 0x5A, (byte) 0x23, (byte) 0x47, (byte) 0x13,
+    (byte) 0x17, (byte) 0x15, (byte) 0x32, (byte) 0x5C, (byte) 0x77, (byte) 0xC9, (byte) 0x73, (byte) 0x04, (byte) 0x2D,
+    (byte) 0x40, (byte) 0x0F, (byte) 0xA5, (byte) 0xA6, (byte) 0x43, (byte) 0x77, (byte) 0x33, (byte) 0x3B, (byte) 0x62,
+    (byte) 0x34, (byte) 0xB6, (byte) 0x72, (byte) 0x32, (byte) 0xB3, (byte) 0xA4, (byte) 0x4B, (byte) 0x80, (byte) 0x7F,
+    (byte) 0xC5, (byte) 0x43, (byte) 0x23, (byte) 0x47, (byte) 0x13, (byte) 0xB7, (byte) 0xA5, (byte) 0x32, (byte) 0xDC,
+    (byte) 0x79, (byte) 0x19, (byte) 0xB1, (byte) 0x03, (byte) 0x9D};
+  static final int BULKING_UP_FACTOR = 16;
+  static final byte[] SECURE_DELETE_FILL_BYTES = new byte[NONSENSE_BYTES.length * BULKING_UP_FACTOR];
+
+  static {
+    // Make some SECURE_DELETE_FILL_BYTES bytes = x BULKING_UP_FACTOR the
+    // NONSENSE just to save write time.
+    for (int i = 0; i < BULKING_UP_FACTOR; i++) {
+      System.arraycopy(
+        NONSENSE_BYTES, 0,
+        SECURE_DELETE_FILL_BYTES, NONSENSE_BYTES.length * i,
+        NONSENSE_BYTES.length);
+    }
+  }
+
   /**
    * Utilities have private constructor
    */
@@ -41,7 +66,6 @@ public class SecureFiles {
    * futex_wait_queue_me error on Linux systems (related to /dev/random usage)
    *
    * @param file The file to secure delete
-   *
    * @throws java.io.IOException if the operation fails for any reason
    */
   public static synchronized void secureDelete(File file) throws IOException {
@@ -49,11 +73,52 @@ public class SecureFiles {
     long start = System.currentTimeMillis();
     log.trace("Start of secureDelete");
 
-    fastSecureDelete(file);
+    if (Utils.isWindows()) {
+      // Use slow secure delete
+      slowSecureDelete(file);
+    } else {
+      fastSecureDelete(file);
+    }
     log.trace("End of secureDelete");
     log.debug("Secure delete took {} milliseconds", System.currentTimeMillis() - start);
 
   }
+
+  /**
+     * Delete a file with an overwrite of all of the data.
+     * <p/>
+     * Set bit patterns are used rather than random numbers to avoid a
+     * futex_wait_queue_me error on Linux systems (related to /dev/random usage)
+     *
+     * @param file The file to secure delete
+     *
+     * @throws java.io.IOException if the operation fails for any reason
+     */
+    @SuppressFBWarnings({"DM_GC"})
+    public static synchronized void slowSecureDelete(File file) throws IOException {
+      if (file != null && file.exists()) {
+        try (RandomAccessFile raf = new RandomAccessFile(file, "rws")) {
+          // Prep for file delete as this can be fiddly on Windows
+          // Make sure it is writable and any references to it are garbage
+          // collected and finalized.
+          if (!file.setWritable(true)) {
+            throw new IOException("Could not write to file " + file.getAbsolutePath());
+          }
+          System.gc();
+
+          long length = file.length();
+          raf.seek(0);
+          raf.getFilePointer();
+          int pos = 0;
+          while (pos < length) {
+            raf.write(SECURE_DELETE_FILL_BYTES);
+            pos += SECURE_DELETE_FILL_BYTES.length;
+          }
+        }
+        boolean deleteSuccess = file.delete();
+        log.debug("Result of delete of file '" + file.getAbsolutePath() + "' was " + deleteSuccess);
+      }
+    }
 
   /**
    * An alternative secure delete algorithm from http://www.cafeaulait.org/books/javaio2/ioexamples/14/SecureDelete.java
@@ -64,12 +129,12 @@ public class SecureFiles {
     if (file.exists()) {
       RandomAccessFile raf = null;
       FileChannel channel = null;
+      MappedByteBuffer buffer;
       try {
-        SecureRandom random = new SecureRandom();
         raf = new RandomAccessFile(file, "rw");
         channel = raf.getChannel();
 
-        MappedByteBuffer buffer = channel.map(
+        buffer = channel.map(
                 FileChannel.MapMode.READ_WRITE,
                 0,
                 raf.length()
@@ -78,22 +143,16 @@ public class SecureFiles {
         // Overwrite with random data; one byte at a time
         byte[] data = new byte[1];
         while (buffer.hasRemaining()) {
-          random.nextBytes(data);
+          secureRandom.nextBytes(data);
           buffer.put(data[0]);
         }
 
         // Ensure we push this out to the file system
         buffer.force();
         channel.close();
-
-        // Use JDK7 NIO Files to delete the file since it offers the following benefits:
-        // * best chance at an atomic operation
-        // * relies on native code
-        // * works on Windows
-        boolean deleteSuccess = Files.deleteIfExists(file.toPath());
-
-        log.trace("Result of delete was {} for:\n'{}'", deleteSuccess, file.getAbsolutePath());
       } finally {
+        buffer = null;
+
         if (channel != null) {
           channel.close();
           channel = null;
@@ -103,14 +162,27 @@ public class SecureFiles {
           raf = null;
         }
       }
+
+      // Delete file
+      // Use JDK7 NIO Files to delete the file since it offers the following benefits:
+      // * best chance at an atomic operation
+      // * relies on native code
+      // * works on Windows
+      boolean deleteSuccess = Files.deleteIfExists(file.toPath());
+      log.debug("Result of initial delete was {} for:\n'{}'", deleteSuccess, file.getAbsolutePath());
+
+      if (Utils.isWindows()) {
+        // Work around an issue on Windows whereby files are not deleted
+        File canonical = file.getCanonicalFile();
+        if (canonical.exists() && !canonical.delete())
+          log.debug("Failed to delete canonical file {}", file.getCanonicalPath());
+      }
     }
   }
 
   /**
    * @param directory The directory to verify or create
-   *
    * @return The directory
-   *
    * @throws java.lang.IllegalStateException If the file could not be created
    */
   public static File verifyOrCreateDirectory(File directory) {
@@ -129,9 +201,7 @@ public class SecureFiles {
   /**
    * @param parentDirectory The parent directory
    * @param childDirectory  The child directory (will be created if absent)
-   *
    * @return The child directory
-   *
    * @throws java.lang.IllegalStateException If the file could not be created
    */
   public static File verifyOrCreateDirectory(File parentDirectory, String childDirectory) {
@@ -157,9 +227,7 @@ public class SecureFiles {
    *
    * @param parentDirectory The parent directory
    * @param filename        The filename
-   *
    * @return A File referring to the existent file
-   *
    * @throws java.lang.IllegalStateException If the file could not be created
    */
   public static File verifyOrCreateFile(File parentDirectory, String filename) {
@@ -191,7 +259,6 @@ public class SecureFiles {
    * <p>Atomically create a temporary directory that will be removed when the JVM exits</p>
    *
    * @return A random temporary directory
-   *
    * @throws java.io.IOException If something goes wrong
    */
   public static File createTemporaryDirectory() throws IOException {
@@ -256,9 +323,9 @@ public class SecureFiles {
     Path tempFilePath = tempFile.toPath();
     Path destFilePath = destFile.toPath();
     java.nio.file.Files.move(
-      tempFilePath,
-      destFilePath,
-      StandardCopyOption.REPLACE_EXISTING
+            tempFilePath,
+            destFilePath,
+            StandardCopyOption.REPLACE_EXISTING
     );
 
   }
