17164d27eaeeb428aace5c559f9d44eeb5815dd2
Jim Burton <jim618@fastmail.co.uk>
2015-03-24 17:31:53 +0000

    #464 moved to integration test

--- mbhd-core/src/test/java/org/multibit/hd/integration/BRITExchangeIntegrationTest.java
+++ mbhd-core/src/test/java/org/multibit/hd/integration/BRITExchangeIntegrationTest.java
@@ -1,10 +1,10 @@
-package org.multibit.hd.core.services;
+package org.multibit.hd.integration;
 
 import com.google.common.base.Optional;
 import org.bitcoinj.core.Wallet;
 import org.bitcoinj.crypto.MnemonicCode;
 import org.junit.Before;
-import org.junit.Test;
+import org.junit.Ignore;
 import org.multibit.hd.brit.dto.FeeState;
 import org.multibit.hd.brit.dto.MatcherResponse;
 import org.multibit.hd.brit.dto.SendFeeDto;
@@ -18,60 +18,16 @@ import org.multibit.hd.core.managers.BackupManager;
 import org.multibit.hd.core.managers.HttpsManager;
 import org.multibit.hd.core.managers.InstallationManager;
 import org.multibit.hd.core.managers.WalletManager;
+import org.multibit.hd.core.services.CoreServices;
 import org.multibit.hd.core.utils.Dates;
 
 import java.io.File;
 
 import static org.fest.assertions.Assertions.assertThat;
 
-public class CoreServicesTest {
+public class BRITExchangeIntegrationTest {
 
   private static final String PASSWORD = "bingAbongADingDong";
-
-//  @Test
-//     public void testCalculateFeeStateWithRealURL() throws Exception {
-//
-//       // Get the FeeService
-//       FeeService feeService = BRITServices.newFeeService(encryptionKey, new URL(CoreServices.));
-//       assertThat(feeService).isNotNull();
-//
-//       // Perform an exchange with the BRIT Matcher to get the list of fee addresses
-//       feeService.performExchangeWithMatcher(seed, wallet1);
-//       assertThat(wallet1.getExtensions().get(MatcherResponseWalletExtension.MATCHER_RESPONSE_WALLET_EXTENSION_ID)).isNotNull();
-//
-//       // Calculate the fee state for an empty wallet
-//       FeeState feeState = feeService.calculateFeeState(wallet1, false);
-//       assertThat(feeState).isNotNull();
-//
-//       // We are using a dummy Matcher so will always fall back to the hardwired addresses
-//       Set<Address> possibleNextFeeAddresses = feeService.getHardwiredFeeAddresses();
-//
-//       checkFeeState(feeState, true, 0, Coin.ZERO, FeeService.FEE_PER_SEND, possibleNextFeeAddresses);
-//
-//       // Receive some bitcoin to the wallet1 address
-//       receiveATransaction(wallet1, toAddress1);
-//
-//       final int NUMBER_OF_NON_FEE_SENDS = 40;
-//       for (int i = 0; i < NUMBER_OF_NON_FEE_SENDS; i++) {
-//         // Create a send to the non fee destination address
-//         // This should increment the send count and the fee owed
-//         Coin tenMillis = parseCoin("0.01");
-//         sendBitcoin(tenMillis, nonFeeDestinationAddress, null);
-//
-//         feeState = feeService.calculateFeeState(wallet1, false);
-//
-//         checkFeeState(feeState, true, 1 + i, FeeService.FEE_PER_SEND.multiply(i + 1), FeeService.FEE_PER_SEND, possibleNextFeeAddresses);
-//       }
-//
-//       // Create another send to the FEE address
-//       // Pay the feeOwed and another fee amount (to pay for this send)
-//       // This should reset the amount owed and create another feeAddress
-//       sendBitcoin(feeState.getFeeOwed().add(FeeService.FEE_PER_SEND), feeState.getNextFeeAddress(), null);
-//
-//       feeState = feeService.calculateFeeState(wallet1, false);
-//       checkFeeState(feeState, true, NUMBER_OF_NON_FEE_SENDS + 1, Coin.ZERO, FeeService.FEE_PER_SEND, possibleNextFeeAddresses);
-//     }
-
 
   @Before
   public void setUp() throws Exception {
@@ -79,14 +35,14 @@ public class CoreServicesTest {
   }
 
   /**
-   * Run a test that exercise the BRIT exchange with a Matcher on multibit.org
+   * Run an integration  test that exercise the BRIT exchange with a Matcher on multibit.org
    *
    * If you are NOT running the BRIT server the exchange fails and the feeService should return the
    * hardwired list of addresses (which will then be used for sending fees to).
    * @throws Exception
    *
    */
-  @Test
+  @Ignore
   public void testCreateFeeServiceAndPerformBRITExchange() throws Exception {
     FeeService feeService = CoreServices.createFeeService();
     assertThat(feeService).isNotNull();
