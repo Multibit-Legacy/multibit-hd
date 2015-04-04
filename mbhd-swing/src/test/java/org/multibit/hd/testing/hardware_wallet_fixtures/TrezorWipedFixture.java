1e0dd64c149cc4f1709f15e7fdf92284f31164b3
Gary Rowe <g.rowe@froot.co.uk>
2015-04-04 21:02:31 +0100

    #443 Updating "create Trezor cold start" FEST test

--- mbhd-swing/src/test/java/org/multibit/hd/testing/hardware_wallet_fixtures/TrezorWipedFixture.java
+++ mbhd-swing/src/test/java/org/multibit/hd/testing/hardware_wallet_fixtures/TrezorWipedFixture.java
@@ -15,7 +15,6 @@ import org.multibit.hd.hardware.core.messages.Features;
 import org.multibit.hd.hardware.core.messages.HardwareWalletMessage;
 import org.multibit.hd.hardware.core.messages.PublicKey;
 import org.multibit.hd.hardware.trezor.clients.AbstractTrezorHardwareWalletClient;
-import org.multibit.hd.testing.MessageEventFixtures;
 
 import java.util.List;
 
@@ -321,25 +320,8 @@ public class TrezorWipedFixture extends AbstractHardwareWalletFixture {
 
     when(client.getDeterministicHierarchy(anyListOf(ChildNumber.class))).thenAnswer(
       new Answer<Optional<Message>>() {
-        private int count = 0;
 
         public Optional<Message> answer(InvocationOnMock invocation) throws Throwable {
-
-          if (count == 0) {
-
-            count++;
-
-            final MessageEvent event = new MessageEvent(
-              MessageEventType.PIN_MATRIX_REQUEST,
-              Optional.<HardwareWalletMessage>of(MessageEventFixtures.newCurrentPinMatrix()),
-              Optional.<Message>absent()
-            );
-
-            fireMessageEvent("Deterministic hierarchy is protected (1.3.3+ firmware). Provide PIN.", event);
-
-            return Optional.absent();
-
-          }
 
           // This unchecked cast is known to be OK
           List<ChildNumber> childNumberList = (List<ChildNumber>) invocation.getArguments()[0];
