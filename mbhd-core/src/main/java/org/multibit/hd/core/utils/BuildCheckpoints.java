17a97d370a3fdef9308cc02e61dea2654f6aea53
Jim Burton <jim618@fastmail.co.uk>
2015-04-07 15:25:32 +0100

    findbugs fix

--- mbhd-core/src/main/java/org/multibit/hd/core/utils/BuildCheckpoints.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/utils/BuildCheckpoints.java
@@ -103,27 +103,34 @@ public class BuildCheckpoints {
     }
 
     private static void writeBinaryCheckpoints(TreeMap<Integer, StoredBlock> checkpoints, File file) throws Exception {
-        final FileOutputStream fileOutputStream = new FileOutputStream(file, false);
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        final DigestOutputStream digestOutputStream = new DigestOutputStream(fileOutputStream, digest);
-        digestOutputStream.on(false);
-        final DataOutputStream dataOutputStream = new DataOutputStream(digestOutputStream);
-        dataOutputStream.writeBytes("CHECKPOINTS 1");
-        dataOutputStream.writeInt(0);  // Number of signatures to read. Do this later.
-        digestOutputStream.on(true);
-        dataOutputStream.writeInt(checkpoints.size());
-        ByteBuffer buffer = ByteBuffer.allocate(StoredBlock.COMPACT_SERIALIZED_SIZE);
-        for (StoredBlock block : checkpoints.values()) {
-            block.serializeCompact(buffer);
-            dataOutputStream.write(buffer.array());
-            buffer.position(0);
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(file, false);
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            final DigestOutputStream digestOutputStream = new DigestOutputStream(fileOutputStream, digest);
+            digestOutputStream.on(false);
+            final DataOutputStream dataOutputStream = new DataOutputStream(digestOutputStream);
+            dataOutputStream.writeBytes("CHECKPOINTS 1");
+            dataOutputStream.writeInt(0);  // Number of signatures to read. Do this later.
+            digestOutputStream.on(true);
+            dataOutputStream.writeInt(checkpoints.size());
+            ByteBuffer buffer = ByteBuffer.allocate(StoredBlock.COMPACT_SERIALIZED_SIZE);
+            for (StoredBlock block : checkpoints.values()) {
+                block.serializeCompact(buffer);
+                dataOutputStream.write(buffer.array());
+                buffer.position(0);
+            }
+            dataOutputStream.close();
+            Sha256Hash checkpointsHash = new Sha256Hash(digest.digest());
+            System.out.println("Hash of checkpoints data is " + checkpointsHash);
+            digestOutputStream.close();
+            fileOutputStream.close();
+            System.out.println("Checkpoints written to '" + file.getCanonicalPath() + "'.");
+        } finally {
+            if (fileOutputStream != null) {
+                fileOutputStream.close();
+            }
         }
-        dataOutputStream.close();
-        Sha256Hash checkpointsHash = new Sha256Hash(digest.digest());
-        System.out.println("Hash of checkpoints data is " + checkpointsHash);
-        digestOutputStream.close();
-        fileOutputStream.close();
-        System.out.println("Checkpoints written to '" + file.getCanonicalPath() + "'.");
     }
 
     private static void writeTextualCheckpoints(TreeMap<Integer, StoredBlock> checkpoints, File file) throws IOException {
