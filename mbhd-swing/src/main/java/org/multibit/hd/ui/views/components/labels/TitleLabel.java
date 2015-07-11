9f46f5afb9f41d035957e51b896facef3eab0aa7
gary.rowe <g.rowe@froot.co.uk>
2015-07-11 21:35:17 +0100

    #384 Added Windows antialiasing incantation

--- mbhd-swing/src/main/java/org/multibit/hd/ui/views/components/labels/TitleLabel.java
+++ mbhd-swing/src/main/java/org/multibit/hd/ui/views/components/labels/TitleLabel.java
@@ -1,5 +1,7 @@
 package org.multibit.hd.ui.views.components.labels;
 
+import sun.swing.SwingUtilities2;
+
 import javax.swing.*;
 import java.awt.*;
 
@@ -15,9 +17,11 @@ public class TitleLabel extends JLabel {
   @Override
   protected void paintComponent(Graphics g) {
 
-    // Prepare the rendering hints
-    ((Graphics2D) g).setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-
+    // Fixes font antialias problem on Windows
+    // Note: Must be this exact reference or it doesn't work
+    // e.g. using new StringBuffer("AATextInfoPropertyKey") fails
+    // See StackOverflow: http://stackoverflow.com/questions/2266199/how-to-make-jtextpane-paint-anti-aliased-font
+    this.putClientProperty(SwingUtilities2.AA_TEXT_PROPERTY_KEY, null);
     super.paintComponent(g);
   }
 }
