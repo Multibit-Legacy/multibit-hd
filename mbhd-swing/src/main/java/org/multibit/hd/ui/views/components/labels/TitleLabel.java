11ef1541f2cf0fdb9a5b1c69e028e6924906c9a9
Gary Rowe <gary.rowe@metoffice.gov.uk>
2015-07-13 18:24:44 +0100

    #660 Avoid anti-aliasing code in Impact Regular

--- mbhd-swing/src/main/java/org/multibit/hd/ui/views/components/labels/TitleLabel.java
+++ mbhd-swing/src/main/java/org/multibit/hd/ui/views/components/labels/TitleLabel.java
@@ -1,5 +1,8 @@
 package org.multibit.hd.ui.views.components.labels;
 
+import org.multibit.hd.core.config.Configurations;
+import org.multibit.hd.ui.languages.LanguageKey;
+import org.multibit.hd.ui.views.fonts.TitleFontDecorator;
 import sun.swing.SwingUtilities2;
 
 import javax.swing.*;
@@ -10,8 +13,16 @@ import java.awt.*;
  */
 public class TitleLabel extends JLabel {
 
+  private final boolean useAATextProperty;
+
   public TitleLabel(String htmlText) {
     super(htmlText);
+
+    // Determine if anti-aliasing can be used
+    Font titleFont  = LanguageKey.fromLocale(Configurations.currentConfiguration.getLocale()).getTitleFont();
+    useAATextProperty = titleFont.equals(TitleFontDecorator.CORBEN_REGULAR)
+    || titleFont.equals(TitleFontDecorator.OPENSANS_SEMIBOLD);
+
   }
 
   @Override
@@ -21,7 +32,9 @@ public class TitleLabel extends JLabel {
     // Note: Must be this exact reference or it doesn't work
     // e.g. using new StringBuffer("AATextInfoPropertyKey") fails
     // See StackOverflow: http://stackoverflow.com/questions/2266199/how-to-make-jtextpane-paint-anti-aliased-font
-    this.putClientProperty(SwingUtilities2.AA_TEXT_PROPERTY_KEY, null);
+    if (useAATextProperty) {
+      this.putClientProperty(SwingUtilities2.AA_TEXT_PROPERTY_KEY, null);
+    }
     super.paintComponent(g);
   }
 }
