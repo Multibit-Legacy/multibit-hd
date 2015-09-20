b4574bd20bca63ff09e8d91b93338293582e21be
Gary Rowe <g.rowe@froot.co.uk>
2015-09-20 12:56:35 +0100

    Add support for brand names in message keys to reduce translation burden

--- mbhd-core/src/main/java/org/multibit/hd/core/dto/WalletMode.java
+++ mbhd-core/src/main/java/org/multibit/hd/core/dto/WalletMode.java
@@ -9,26 +9,37 @@ package org.multibit.hd.core.dto;
  * <p>This reduces code complexity in factory methods when deciding how to build supporting objects</p>
  *
  * @since 0.0.1
- *
  */
 public enum WalletMode {
 
   /**
    * Target a standard soft wallet (BIP 32 or BIP 44)
    */
-  STANDARD,
+  STANDARD("MultiBit"),
 
   /**
    * Target a Trezor wallet (BIP 44 only)
    */
-  TREZOR,
+  TREZOR("Trezor"),
 
   /**
    * Target a KeepKey wallet (BIP 44 only)
    */
-  KEEP_KEY,
+  KEEP_KEY("KeepKey"),
 
   // End of enum
   ;
 
+  private final String brand;
+
+  WalletMode(String brand) {
+    this.brand = brand;
+  }
+
+  /**
+   * @return The brand name for use with localisation
+   */
+  public String brand() {
+    return brand;
+  }
 }
